f01a90b7e40a8727556ef5269fc34f8573d4877b
Szilagyi Kinga <szilagyikinga42@gmail.com>
2022-01-02 17:41:39 +0100

    feat(client): setup admin with create react app

---
+++ admin/src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://localhost:8080/api');
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="establishment" list={ListGuesser} />
+  </Admin>
+);
+
+export default App;
