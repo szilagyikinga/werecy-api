ee388b252808244c7309f7b82254e9f4a7d97f2b
Geoffrey Hervet <hervetg@gmail.com>
2022-01-05 22:24:58 +0100

    refactor: made list re-usable

--- resources/establishment/establishment.api.controllers.js
+++ resources/establishment/establishment.api.controllers.js
@@ -1,25 +1,6 @@
 const model = require('./establishment.model');
+const routerGenerator = require('../../utils/reactAdmin');
 
-const getMany = async (req, res, next) => {
-  try {
-    const { _end, _order, _sort, _start, q } = req.query;
-    const [establishments, count] = await Promise.all([
-      model.Establishment.find({})
-        .limit(Number.parseInt(_end) - Number.parseInt(_start))
-        .skip(Number.parseInt(_start))
-        .sort(`${_order === 'ASC' ? '-' : '+'}${_sort}`)
-        .exec(),
-      model.Establishment.count({}),
-    ]);
-
-    console.log({ count });
-
-    res.header('X-Total-Count', count);
-    res.status(200).json(establishments);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
+module.exports = {
+  getMany: routerGenerator.list(model.Establishment),
 };
-
-module.exports = { getMany };
