6bb16d113afa2d77cea22799d0a612438e809a42
Geoffrey Hervet <hervetg@gmail.com>
2022-01-18 16:46:18 +0100

    feat: upload image via base64

--- resources/establishment/establishment.api.router.js
+++ resources/establishment/establishment.api.router.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const { Establishment } = require('./establishment.model');
-const controller = require('../../utils/reactAdmin')(Establishment);
+const controllerGenerator = require('../../utils/reactAdmin');
+const uuid = require('uuid').v4;
+const cloudinary = require('cloudinary').v2;
+
+const upload = async (base64, type) =>
+  new Promise((resolve, reject) =>
+    cloudinary.uploader.upload(
+      `data:${type};base64,${base64}`,
+      {
+        tags: 'basic_sample',
+        public_id: uuid(),
+      },
+      (err, image) => (err ? reject(err) : resolve(image.secure_url.split('/').slice(-1)))
+    )
+  );
+
+const transformer = async ({ image, logo, ...dataToWrite }) => {
+  const data = {
+    ...dataToWrite,
+    image: typeof image !== 'string' ? await upload(image) : image,
+    logo: typeof logo !== 'string' ? await upload(logo) : logo,
+  };
+
+  console.log(data);
+  return data;
+};
+
+const controller = controllerGenerator(Establishment, transformer);
 
 router.get('/', controller.list);
 router.post('/', controller.create);
