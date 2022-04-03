3d1daca31db97855a02b8ce09e79d7708e44405e
Szilagyi Kinga <szilagyikinga42@gmail.com>
2022-04-03 10:01:51 +0200

    fix: remove obsolete create route

--- resources/collectingPoint/collectingPoint.app.controllers.js
+++ resources/collectingPoint/collectingPoint.app.controllers.js
@@ -1,10 +1,4 @@
 const model = require('./collectingPoint.model');
-const bannerModel = require('../banner/banner.model');
-const collectingModel = require('../collecting/collecting.model');
-const establishmentModel = require('../establishment/establishment.model');
-const list = require('./collectingPoint.data');
-const bannerList = require('../banner/banner.data');
-const establismentList = require('../establishment/establishment.data');
 const appConfig = require('../../config/appConfig');
 const dateService = require('../../services/date');
 
@@ -70,57 +64,4 @@ const getMany = (req, res) => {
     });
 };
 
-const addMany = async (req, res) => {
-  try {
-    await model.CollectingPoint.deleteMany({});
-    await bannerModel.Banner.deleteMany({});
-    await collectingModel.Collecting.deleteMany({});
-    await establishmentModel.Establishment.deleteMany({});
-
-    // Establishments
-    const formattedEstablismentList = establismentList.map(({ coordinates, ...rest }) => ({
-      ...rest,
-      location: {
-        type: 'Point',
-        coordinates: [coordinates.longitude, coordinates.latitude],
-      },
-    }));
-    const estabmlisments = await establishmentModel.Establishment.create(formattedEstablismentList);
-
-    // Collecting points
-    const formattedCollectingPointList = list.map(({ establishment, startDate, endDate, ...rest }) => {
-      const currentEstablishment = estabmlisments.find((e) => e.code === establishment);
-      if (!currentEstablishment) throw new Error();
-      return {
-        establishment: currentEstablishment._id,
-        startDate: startDate ? new Date(startDate) : undefined,
-        endDate: endDate ? new Date(endDate) : undefined,
-        types: currentEstablishment.types,
-        location: {
-          type: 'Point',
-          coordinates: [currentEstablishment.location.coordinates[0], currentEstablishment.location.coordinates[1]],
-        },
-        ...rest,
-      };
-    });
-    const collectingPoints = await model.CollectingPoint.create(formattedCollectingPointList);
-
-    // Banner
-    bannerList.forEach(({ establishment: establishmentCode, startDate, endDate, ...rest }) => {
-      const establishment = estabmlisments.find((e) => e.code === establishmentCode);
-      bannerModel.Banner.create({
-        startDate: new Date(startDate),
-        endDate: new Date(endDate),
-        establishment: establishment._id,
-        ...rest,
-      });
-    });
-
-    res.status(201).json({ data: estabmlisments });
-  } catch (e) {
-    console.error(e);
-    res.status(400).end();
-  }
-};
-
-module.exports = { getMany, addMany };
+module.exports = { getMany };
--- resources/collectingPoint/collectingPoint.app.router.js
+++ resources/collectingPoint/collectingPoint.app.router.js
@@ -3,6 +3,5 @@ const controller = require('./collectingPoint.app.controllers');
 const router = express.Router();
 
 router.get('/', controller.getMany);
-// router.get('/add', controller.addMany);
 
 module.exports = router;
